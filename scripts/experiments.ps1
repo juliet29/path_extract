bf32f62666215f1b4855554d0aa01aa0586d868e
Juliet Nwagwu Ume-Ezeoke <jul29iet@gmail.com>
2025-07-03 15:01:01 -0700

    better way to write script

--- scripts/experiments.ps1
+++ scripts/experiments.ps1
@@ -1,6 +1,36 @@
-. .\setup.ps1
-# New-Setup "Pier 6" "EDC original scope\Page {0} Pathfinder.txt" "EDC original scope" 0;
-# New-Setup "Pier 6" "Entire project\Page {0} Pathfinder.txt" "Entire project" 1;
-# New-Setup "Pier 6" "Landscape only\Page {0} Pathfinder.txt" "Landscape only" 2;
+# . .\setup.ps1
+# $pier_6 = @{
+#     0 = 'EDC original scope'
+#     1 = 'Entire project'
+#     2 = 'Landscape only'
+# }
+# for (($i = 0);  ($i -lt $pier_6.Count);, ($i++)){
+#     $value = $pier_6[$i]
+#     New-Setup "Pier 6" "$value\Page {0} Pathfinder.txt" $value $i;
+# }
 
-New-Setup "Newtown Creek" "Newtown-Creek-page_{0}.txt" "Baseline" 0;
+
+
+$newtown_creek = @{
+    0 = 'baseline'
+}
+for (($i = 0);  ($i -lt $newtown_creek.Count);, ($i++)){
+    $value = $newtown_creek[$i]
+    New-Setup "Newtown Creek" "Newtown-Creek-page_{0}.txt" $value $i;
+}
+
+
+
+# TODO make this work!
+# function New-Project {
+#     param (
+#         [hashtable] $NamesDict,
+#         [string] $FilePattern,
+#         [string] $ProjectName
+#     )
+
+#     for (($i = 0);  ($i -lt $NamesDict.Count);, ($i++)){
+#     $value = $NamesDict[$i]
+# 	New-Setup "Pier 6" "$value\Page {0} Pathfinder.txt" $value $i;
+# }
+# }
